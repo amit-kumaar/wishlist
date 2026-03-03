b576d28fec222fb1dad6763cc5f27fd979f798f2
amit-kumaar <amitk.umar@yahoo.com>
2026-03-03 10:58:23 +0530

    binding event

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { WishItem}  from '../shared/models/wishItems';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('wishlist');
+  items:WishItem[]=[
+    new WishItem('To learn angular'),
+    new WishItem('Get coffee', true),
+    new WishItem('Find grass that cuts itself')
+  ];
+  toggleItem(item:WishItem){
+    item.isComplete=!item.isComplete;
+    console.log(item)
+  }
 }
