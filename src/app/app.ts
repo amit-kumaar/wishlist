7d2242de7ed52f53a9b1e1819698798a0180b6d0
amit-kumaar <amitk.umar@yahoo.com>
2026-03-04 23:46:58 +0530

    day-2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { WishItem}  from '../shared/models/wishItems';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -15,6 +17,20 @@ export class App {
     new WishItem('Get coffee', true),
     new WishItem('Find grass that cuts itself')
   ];
+  listFilter:string='0';
+  newWishText='';
+
+
+
+
+  addNewWish(){
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText='';
+  }
+
+  filterChanged(value:any){
+    console.log(value)
+  }
   toggleItem(item:WishItem){
     item.isComplete=!item.isComplete;
     console.log(item)
